dcaedffaed26bfebe4e9de390bd788786310ec1c
pawansubedi4 <pawan.13pro@gmail.com>
2024-07-16 14:39:26 +0545

    pythen 2nd class

---
+++ marks.py
@@ -0,0 +1,11 @@
+m1=int(input("enter your marks in 1st subject "))
+m2=int(input("enter your marks in 1st subject "))
+m3=int(input("enter your marks in 1st subject "))
+m4=int(input("enter your marks in 1st subject "))
+m5=int(input("enter your marks in 1st subject "))
+sum=m1+m2+m3+m4+m5
+tt=int(input("enter your total marks "))
+
+per=sum/tt*100
+print("the total is",sum)
+print("the percentage is ",per)
---
+++ name.py
@@ -0,0 +1,5 @@
+name =input("enter your name")
+email =input("enter your email")
+phone =input("enter your phone no")
+address=input("enter your address")
+print(f"name= {name} \n email= {email} \n phone={phone} \n adress={address}")
