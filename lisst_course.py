4a18eba51303204d818e47ef4e6b998a0840803c
pawansubedi4 <pawan.13pro@gmail.com>
2024-07-17 17:01:33 +0545

    try 56

---
+++ lisst_course.py
@@ -0,0 +1,19 @@
+data=[5,6,7,8,9,10]
+print(data[1],data[5])
+data.append(900)
+data.insert(2,100)
+print(data.count(5))
+print(data)
+data.remove(5)
+print(data)
+
+data1=[
+    [45,65,76],
+    [65,56,78]
+]
+print(data1[1][0])
+
+
+
+
+    
---
+++ namefd.py
@@ -0,0 +1,8 @@
+student=[]
+name=input("enter your name")
+#student.append(name)
+email=input("enter your email")
+adderess=input("enter your address")
+phone=input("enter your phone")
+student.extend([name,email,adderess,phone])
+print(student)
