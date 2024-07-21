68ac0a09d7c23d9194d889f4667466ff67433529
pawansubedi4 <pawan.13pro@gmail.com>
2024-07-21 17:17:59 +0545

    hellow

---
+++ divisible by 3 and 5.py
@@ -0,0 +1,6 @@
+no=int(input("enter any no"))
+if no%3==0 and no%5==0:
+    print("it is divisible")
+else:
+
+    print("it is not divisible")
---
+++ voting check.py
@@ -0,0 +1,7 @@
+age=int(input("enter your age"))
+if age<18:
+    print("you are child")
+elif age>40:
+    print("you are older")
+else:
+    print("you are eligible for vote")    
