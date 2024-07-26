f2dc62874322957f9b6a88ecc438a770d33205d8
pawansubedi4 <pawan.13pro@gmail.com>
2024-07-26 13:49:07 +0545

    python classs

---
+++ for loop.py
@@ -0,0 +1,18 @@
+fruit=["apple","orange","graphes","banana"]
+for banana in fruit:
+    print(banana)
+    print("my hai")
+
+    Pawan=[1,2,3]
+    for i in Pawan:
+        print(i)
+hi=("pawan","tripti","prabin","pabi","laxami","hemanta")
+x=0
+while x<=5:
+    print(hi[x])
+    x+=1
+for x in hi:
+    print(x)    
+my_tuple = (1, "apple", 3.14)
+for item in my_tuple:
+    print(item)                  
